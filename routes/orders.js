c4d8231cfe9cb781a7bcc148e0451f3d62cf6dbe
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-02-02 16:02:36 +0100

    add get all posts route

--- routes/orders.js
+++ routes/orders.js
@@ -3,4 +3,31 @@ const router = express.Router();
 
 const pool = require("../dbconfig");
 
+const populate = async (order) => {
+  const userRequest = await pool.query("SELECT * FROM users WHERE id = $1", [
+    order.user_id,
+  ]);
+  order.user = userRequest.rows[0];
+  delete order.user_id;
+  return order;
+};
+
+router.get("/", async (req, res) => {
+  console.log("request on all posts");
+  try {
+    const data = await pool.query("SELECT * FROM orders");
+    const populatedOrders = await Promise.all(
+      data.rows.map((order) => populate(order))
+    );
+    res.json({
+      operation: "success",
+      description: "fetched orders by id",
+      data: populatedOrders,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(400).send();
+  }
+});
+
 module.exports = router;
