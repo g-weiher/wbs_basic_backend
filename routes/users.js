2eb00598638349f7a435d45dff059260b29cbe3f
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-02-02 16:01:40 +0100

    add user by id route

--- routes/users.js
+++ routes/users.js
@@ -19,4 +19,31 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  const userId = req.params.id;
+  console.log(`request on user with id = ${userId}`);
+  if (!(userId && /^\d+$/.test(userId))) {
+    res.status(400).send();
+  } else {
+    try {
+      const data = await pool.query("SELECT * FROM users WHERE id = $1", [
+        userId,
+      ]);
+      if (data.rows.length === 0) {
+        res.status(404).send();
+      } else {
+        res.send({
+          operation: "success",
+          code: 200,
+          description: "fetched user by id",
+          data: data.rows[0],
+        });
+      }
+    } catch (e) {
+      console.error(e);
+      res.status(400).send("something went wrong");
+    }
+  }
+});
+
 module.exports = router;
