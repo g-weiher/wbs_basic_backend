cc02173ff8e738488cdcf35992cb712e72fc4abd
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-02-02 16:01:12 +0100

    add allUsers route

--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,20 @@ const router = express.Router();
 
 const pool = require("../dbconfig");
 
+router.get("/", async (req, res) => {
+  console.log("request on all users");
+  try {
+    const data = await pool.query("SELECT * FROM users");
+    res.json({
+      operation: "success",
+      code: 200,
+      description: "fetched user by id",
+      data: data.rows,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(400).send();
+  }
+});
+
 module.exports = router;
