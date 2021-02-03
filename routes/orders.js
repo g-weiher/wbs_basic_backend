a9dc5c841460063aa5958100aa02390068fc91f7
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-02-03 10:04:51 +0100

    switch populating from api side to db side

--- routes/orders.js
+++ routes/orders.js
@@ -3,26 +3,16 @@ const router = express.Router();
 
 const pool = require("../dbconfig");
 
-const populate = async (order) => {
-  const userRequest = await pool.query("SELECT * FROM users WHERE id = $1", [
-    order.user_id,
-  ]);
-  order.user = userRequest.rows[0];
-  delete order.user_id;
-  return order;
-};
-
 router.get("/", async (req, res) => {
   console.log("request on all orders");
   try {
-    const data = await pool.query("SELECT * FROM orders");
-    const populatedOrders = await Promise.all(
-      data.rows.map((order) => populate(order))
+    const data = await pool.query(
+      "SELECT orders.id, orders.price, orders.date, row_to_json(users.*) AS user FROM orders, users WHERE orders.user_id = users.id"
     );
     res.json({
       operation: "success",
       description: "fetched orders by id",
-      data: populatedOrders,
+      data: data.rows,
     });
   } catch (e) {
     console.error(e);
